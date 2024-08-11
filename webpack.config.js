03f77c6b553d18717c70613dc9eef6ddc3b6ee71
Ashok <ashok@N14545.local>
2024-08-11 11:42:16 +0530

    webpack config changes

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        library: 'react-virtual-scroll-lists',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
+};
